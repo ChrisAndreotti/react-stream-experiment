f4b8487fd0d06916a48e5a06ef9959a278ffe747
Chris Andreotti <chris.andreotti@gmail.com>
2019-02-02 18:20:23 -0500

    Creates component scaffolding and defines routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import { MemoryRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-    return (
-        <div>
-            <h1>Page One</h1>
-            <Link to='/pagetwo'>page two</Link>
-        </div>
-    );
-};
-
-const PageTwo = () => {
-    return (
-        <div>
-            <h1>Page Two</h1>
-            <Link to='/'>page one</Link>
-        </div>
-    );
-};
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
         <div>
-            <MemoryRouter>
+            <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </MemoryRouter>
+            </BrowserRouter>
         </div>
     );
 };
