1bf565553c4be9cf65523996f058f18fd34bdc53
Chris Andreotti <chris.andreotti@gmail.com>
2019-02-03 11:45:17 -0500

    Adds call to get auth instance from Google API, setting isSignedIn to local component state

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,6 +3,8 @@ import React from 'react';
 class GoogleAuth extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { isSignedIn: null };
+
         this.gApiApplicationClientId = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_GOOGLE_OAUTH_CLIENT_ID : process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
     }
 
@@ -11,13 +13,25 @@ class GoogleAuth extends React.Component {
             window.gapi.client.init({
                 clientId: this.gApiApplicationClientId,
                 scope: 'email',
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
             });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null ) {
+            return <div>I don't know if we are signed in</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in!</div>;
+        } else {
+            return <div>I am not signed in</div>;
+        }
+    }
+
     render() {
-        console.log(process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID);
-        return <div>Google Auth</div>;
+        return <div>{ this.renderAuthButton() }</div>;
     }
 }
 
