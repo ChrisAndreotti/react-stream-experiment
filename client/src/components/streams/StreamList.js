a677b23450e6a11ca0cdfe747b427421896a6857
Chris Andreotti <chris.andreotti@gmail.com>
2019-03-17 19:02:44 -0400

    Adds a Create Stream button that appears when a user is logged in. Linked to the create stream route using the Link component of react-router-dom

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
     componentDidMount() {
@@ -32,6 +33,18 @@ class StreamList extends React.Component {
             );
         });
     }
+
+    renderCreateButton() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
     
     render() {
         return (
@@ -40,6 +53,7 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     { this.renderList() }
                 </div>
+                { this.renderCreateButton() }
             </div>
         );
     }
@@ -49,6 +63,7 @@ const mapStateToProps = state => {
     return { 
         streams: Object.values(state.streams),
         currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn,
     };
 };
 
