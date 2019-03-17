0c05f0106417e197168b83bcaa5c5d2ca5d2fe62
Chris Andreotti <chris.andreotti@gmail.com>
2019-03-17 18:49:49 -0400

    Updates StreamList component to get the current userId. For each stream, compare the current userId with the stream.userId property to determine if the edit and delete buttons should be displayed

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,10 +7,22 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdminButtons(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            );
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdminButtons(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -36,6 +48,7 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
     return { 
         streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
     };
 };
 
